4b6cbda880d351b8c38b4571e957f868d3da26fe
Takeru Suzuki <terkeljp@gmail.com>
2017-09-27 08:22:52 +0900

    v0.5.0

--- mojik.js
+++ mojik.js
@@ -1,5 +1,5 @@
 /*!
- * Mojik v0.4.0 https://github.com/terkel/mojik
+ * Mojik v0.5.0 https://github.com/terkel/mojik
  * @author Takeru Suzuki http://terkel.jp/
  * @license MIT http://opensource.org/licenses/MIT
  */
